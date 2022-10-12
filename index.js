2a763186835db4b231305001761328dc848c2f65
bange-git <sebastienbange@gmail.com>
2022-10-12 03:43:10 +0100

    set up of basic node server

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+ if(req.url == '/'){
+    fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+        if(err) throw err;
+        res.writeHead(200, {"content-type": "text/html" });
+        res.end(content);
+    })
+ }
+});
+
+const port = 5000 | process.env.port 
+
+server.listen(port, () => console.log(`server is running on port: ${port}`));
