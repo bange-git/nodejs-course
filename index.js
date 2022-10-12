cea9e88b04efcd52417c0de65fb6073054d20a7e
bange-git <sebastienbange@gmail.com>
2022-10-12 03:54:54 +0100

    loading files with different content types

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ const server = http.createServer((req, res) => {
         res.end(content);
     })
  }
+ if(req.url == '/api/users'){
+    const users = [
+        {name: "hanan foe", age: 27},
+        {name: "seb", age: 30}
+    ];
+    res.writeHead(200, {"content-type": "application/json" });
+    res.end(JSON.stringify(users));
+ }
  else if(req.url == '/about') {
     fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
         if(err) throw err;
