b0536d859f1a1de00cde72961bf76b5476fc5cf9
bange-git <sebastienbange@gmail.com>
2022-10-12 03:49:02 +0100

    loading files based on url

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ const server = http.createServer((req, res) => {
         res.end(content);
     })
  }
+ else if(req.url == '/about') {
+    fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
+        if(err) throw err;
+        res.writeHead(200, {"content-type": "text/html" });
+        res.end(content);
+    })
+ }
 });
 
 const port = 5000 | process.env.port 
